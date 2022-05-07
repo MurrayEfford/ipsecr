8838bb00b87748abc739fd2522a2fd4c76ea835f
MurrayEfford <MurrayEfford@users.noreply.github.com>
2022-05-07 12:48:05 +1200

    proxyfn2 for nontarget data

--- R/proxyfn.R
+++ R/proxyfn.R
@@ -51,6 +51,54 @@ proxyfn1 <- function (capthist, N.estimator =  c("n", "null","zippin","jackknife
             Mh(fi)
         }
     }
-    c(logN = log(estimates[1]), hazard = -log(1-estimates[2]), logrpsv= log(RPSV(capthist, CC = TRUE)))
+    c(
+        logN = log(estimates[1]), 
+        hazard = -log(1-estimates[2]), 
+        logrpsv= log(RPSV(capthist, CC = TRUE))
+    )
+}
+##################################################
+
+proxyfn2 <- function (capthist, N.estimator =  c("n", "null","zippin","jackknife")) {
+    N.estimator <- tolower(N.estimator)
+    N.estimator <- match.arg(N.estimator)
+    ## capthist single-session only; ignoring losses
+    n <- nrow(capthist)         ## number of individuals
+    ch <- abs(capthist)>0
+    nocc <- ncol(capthist)      ## number of occasions
+    ni <- apply(ch, 2, sum)     ## individuals on each occasion
+    nontarget <- attr(capthist, 'nontarget', exact = TRUE)
+    if (is.null(nontarget)) {
+        stop ("proxyfn2 requires nontarget")
+    }
+    if (nrow(nontarget)!= dim(capthist)[3] || ncol(nontarget) != nocc) {
+        stop ("invalid nontarget data in proxyfn2")
+    }
+    pdisturb <- mean(nontarget)
+    if (pdisturb>=1) {
+        pdisturb <- 1 - 1e-4  ## arbitrary to dodge log(0)
+    }
+    estimates <- {
+        if (N.estimator == "n")
+            c(n, sum(ni)/n/nocc)
+        else if (N.estimator == "null")
+            M0(c(sum(ni), n, nocc))
+        else if (N.estimator == "zippin") {
+            tempx2 <- apply(ch, 1, function(x) cumsum(abs(x))>0)
+            Mt1 <- apply(tempx2,1,sum)
+            ui <- c(ni[1], diff(Mt1))
+            Mb(ui)
+        }
+        else if (N.estimator == "jackknife") {
+            fi <- tabulate(apply(ch,1,sum), nbins = nocc)
+            Mh(fi)
+        }
+    }
+    c(
+        logN = log(estimates[1]), 
+        hazard = -log(1-estimates[2]), 
+        logrpsv = log(RPSV(capthist, CC = TRUE)),
+        disthazard = log(-log(1-pdisturb))
+    )
 }
 ##################################################
