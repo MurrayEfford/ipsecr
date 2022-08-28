988fe51da13a581fcf0d568f344b62fdd5f2966d
MurrayEfford <MurrayEfford@users.noreply.github.com>
2022-08-28 21:40:53 +1200

    new results

--- .testcoverage.R
+++ .testcoverage.R
@@ -25,3 +25,26 @@ package_coverage()
 # R/simpop.R: 100.00%
 # src/pop.cpp: 100.00%
 # src/RPSV.cpp: 100.00%
+
+## 2022-08-28 2140
+
+# ipsecr Coverage: 61.41%
+# R/plot.ipsecr.R: 0.00%
+# R/plot3D.IP.R: 0.00%
+# R/plotProxy.R: 45.61%
+# R/utility.R: 49.47%
+# R/vcov.ipsecr.R: 54.00%
+# R/makeNewdata.ipsecr.R: 60.87%
+# R/ipsecr.fit.R: 66.08%
+# R/predict.ipsecr.R: 67.65%
+# R/simCH.R: 72.22%
+# src/CH.cpp: 72.38%
+# R/nonspatial.R: 76.34%
+# R/proxyfn.R: 77.08%
+# R/print.ipsecr.R: 79.66%
+# R/summary.ipsecr.R: 87.80%
+# R/coef.ipsecr.R: 92.86%
+# R/simpop.R: 96.88%
+# R/onAttach.R: 100.00%
+# src/pop.cpp: 100.00%
+# src/RPSV.cpp: 100.00%
