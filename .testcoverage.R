b33b4514de5fdb7ecd7beeb997b35001ef9db666
MurrayEfford <MurrayEfford@users.noreply.github.com>
2022-08-28 20:59:56 +1200

    covr test coverage

---
+++ .testcoverage.R
@@ -0,0 +1,27 @@
+# testing coverage 2022-08-24
+library(covr)
+# run from the package source directory
+package_coverage()
+
+## 2022-08-27 0816
+
+# ipsecr Coverage: 61.15%
+# R/plot.ipsecr.R: 0.00%
+# R/plot3D.IP.R: 0.00%
+# R/plotProxy.R: 45.61%
+# R/utility.R: 49.47%
+# R/vcov.ipsecr.R: 54.00%
+# R/makeNewdata.ipsecr.R: 60.87%
+# R/ipsecr.fit.R: 65.81%
+# R/predict.ipsecr.R: 67.65%
+# R/simCH.R: 72.22%
+# src/CH.cpp: 72.38%
+# R/proxyfn.R: 74.82%
+# R/nonspatial.R: 76.34%
+# R/print.ipsecr.R: 79.66%
+# R/summary.ipsecr.R: 87.80%
+# R/coef.ipsecr.R: 92.86%
+# R/onAttach.R: 100.00%
+# R/simpop.R: 100.00%
+# src/pop.cpp: 100.00%
+# src/RPSV.cpp: 100.00%
